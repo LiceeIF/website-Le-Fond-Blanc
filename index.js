f2ee76112ab69801c1b8be90bf083a774ccda0ef
LiceeIF <a.alicemaria.oliveira@gmail.com>
2023-10-14 23:13:07 -0300

    atualização do projeto

--- index.js
+++ index.js
@@ -1,4 +1,4 @@
-document.querySelectorAll('a[href^="#"]').forEach(anchor => {
+/*document.querySelectorAll('a[href^="#"]').forEach(anchor => {
     anchor.addEventListener('click', function(e) {
       e.preventDefault();
   
@@ -12,4 +12,4 @@ document.querySelectorAll('a[href^="#"]').forEach(anchor => {
         });
       }
     });
-  });
+  });*/
